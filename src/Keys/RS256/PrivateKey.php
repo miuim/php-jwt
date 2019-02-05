caa42fc9afb48cf56634edc9cb65f3e3947fb8fa
François Kooman <fkooman@tuxed.net>
2019-02-05 21:48:44 +0100

    update (c) year

--- src/Keys/RS256/PrivateKey.php
+++ src/Keys/RS256/PrivateKey.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Copyright (c) 2018 François Kooman <fkooman@tuxed.net>
+ * Copyright (c) 2019 François Kooman <fkooman@tuxed.net>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
