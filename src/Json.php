a90e574a7b1d5594d75a20184b9ff81fb9aa983a
François Kooman <fkooman@tuxed.net>
2019-02-05 22:10:14 +0100

    update JSON class

--- src/Json.php
+++ src/Json.php
@@ -37,8 +37,14 @@ class Json
     public static function encode(array $jsonData)
     {
         $jsonString = \json_encode($jsonData);
+        // 5.5.0 	The return value on failure was changed from null string to FALSE.
         if (false === $jsonString || 'null' === $jsonString) {
-            throw new JsonException('unable to encode');
+            throw new JsonException(
+                \sprintf(
+                    'unable to encode JSON, error code "%d"',
+                    \json_last_error()
+                )
+            );
         }
 
         return $jsonString;
@@ -54,13 +60,18 @@ class Json
         if (!\is_string($jsonString)) {
             throw new TypeError('argument 1 must be string');
         }
+
         /** @psalm-suppress MixedAssignment */
         $jsonData = \json_decode($jsonString, true);
-        if (null === $jsonData) {
-            if (JSON_ERROR_NONE !== \json_last_error()) {
-                throw new JsonException('unable to decode');
-            }
+        if (null === $jsonData && JSON_ERROR_NONE !== \json_last_error()) {
+            throw new JsonException(
+                \sprintf(
+                    'unable to decode JSON, error code "%d"',
+                    \json_last_error()
+                )
+            );
         }
+
         if (!\is_array($jsonData)) {
             throw new JsonException('not a JSON object');
         }
